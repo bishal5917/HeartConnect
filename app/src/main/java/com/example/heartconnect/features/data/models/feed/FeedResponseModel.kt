97d036641f9052696581ecde6e75c17b47620294
bishal5917 <poudelb172@gmail.com>
2023-09-19 12:25:22 +0545

    custom network image issue solved,refactor on home screen

---
+++ app/src/main/java/com/example/heartconnect/features/data/models/feed/FeedResponseModel.kt
@@ -0,0 +1,25 @@
+package com.example.heartconnect.features.data.models.feed
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+class FeedResponseModel : ArrayList<FeedResponseModelItem>(){
+    @Serializable
+    data class FeedResponseModelItem(
+        @SerialName("birthYear")
+        val birthYear: String?,
+        @SerialName("email")
+        val email: String?,
+        @SerialName("gender")
+        val gender: String?,
+        @SerialName("hobbies")
+        val hobbies: List<String?>?,
+        @SerialName("image")
+        val image: String?,
+        @SerialName("name")
+        val name: String?,
+        @SerialName("phone")
+        val phone: String?
+    )
+}
